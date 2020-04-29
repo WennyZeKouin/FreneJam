8bc805d1a06d4f4356bb4b43f9b9bb5014d40a69
Adrienjura <63359885+Adrienjura@users.noreply.github.com>
2020-04-29 21:41:45 +0200

    sdfg

---
+++ FreneJam/Assets/Trump/Scripts/Agent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agent : MonoBehaviour
+{
+
+    public Transform home;
+    NavMeshAgent agent;
+    
+
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(home.position);
+    }
+
+
+ 
+}
