7588d4191388e45b82bcbebe44cfa0972dadf0bb
Adrienjura <63359885+Adrienjura@users.noreply.github.com>
2020-04-29 22:48:56 +0200

    Left click suprise

---
+++ FreneJam/Assets/Trump/Scripts/Click.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public Camera mainCamera; 
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 touchPos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+            Instantiate(prefab, touchPos, Quaternion.identity);
+        }
+    }
+}
