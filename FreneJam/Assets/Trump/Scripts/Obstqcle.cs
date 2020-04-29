6ab8787b21f6c12fb576d4fbb9d3df49b9516b1f
Adrienjura <63359885+Adrienjura@users.noreply.github.com>
2020-04-29 22:13:03 +0200

    Navmesh Working

---
+++ FreneJam/Assets/Trump/Scripts/Obstqcle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstqcle : MonoBehaviour
+{
+    bool open = false;
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (open)
+                this.transform.Translate(0, 3, 0);
+            else
+                this.transform.Translate(0, -3, 0);
+
+            open = !open;
+        }
+    }
+}
