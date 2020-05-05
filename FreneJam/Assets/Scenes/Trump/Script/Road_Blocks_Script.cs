60ffee1e6da7a8b60f52eb18f8bf7180a688df97
WennyZeKouin <rethoreeloise@gmail.com>
2020-05-05 22:17:21 +0200

    jepush

---
+++ FreneJam/Assets/Scenes/Trump/Script/Road_Blocks_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road_Blocks_Script : MonoBehaviour
+{
+    public GameObject Road_Block;
+    float Time_Left;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time_Left = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time_Left -= Time.deltaTime;
+
+        if (Time_Left <= 0) 
+        {
+            Destroy(this.gameObject);
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Moving_Location>().Available_Units += 5;
+        }
+    }    
+}
---
+++ FreneJam/Assets/Scenes/Trump/Script/TimerTextManagement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerTextManagement : MonoBehaviour
+{
+    public float timerGameOver;
+    public TextMeshProUGUI timerText;
+
+    void Start()
+    {
+        timerGameOver =0f;    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timerGameOver += Time.deltaTime;
+        timerText.GetComponent<TextMeshProUGUI>().text = timerGameOver.ToString();
+        
+    }
+}
